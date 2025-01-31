63ae8bb5ebb61b65bdeafa07d5c8072ac6d4bbe7
herbertmoroni <herbertmoroni@hotmail.com>
2025-01-30 18:48:36 -0700

    adjust temple routes

--- routes/temple.js
+++ routes/temple.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 const temples = require('../controllers/temple.js');
 
-routes.get('/', temples.findAll);
+//routes.get('/', temples.findAll);
 routes.get('/:temple_id', temples.findOne);
 
 routes.post('/', temples.create);
